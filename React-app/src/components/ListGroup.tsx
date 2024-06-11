bd0be168528a6d85d10cfe0400099dc939227acb
Kariuki11 <irungukenneth433@gmail.com>
2024-06-12 02:44:19 +0300

    New changes

--- React-app/src/components/ListGroup.tsx
+++ React-app/src/components/ListGroup.tsx
@@ -1,22 +1,22 @@
 import { Fragment } from "react";
 
 function ListGroup() {
-    const items = [
-        'New York',
-        'Los Angeles',
-        'Chicago',
-        'Houston',
-        'Philadelphia',
-    ];
+    const items = ['New York', 'Los Angeles','Chicago', 'Houston', 'Philadelphia',];
+    items = [];
+
+    if (items.length === 0)
+        return <p>No item found</p>;
+
   return (
     <Fragment>
         <h1>List</h1>
         <ul className="list-group">
-          <li className="list-group-item">An item</li>
-          <li className="list-group-item">A second item</li>
-          <li className="list-group-item">A third item</li>
-          <li className="list-group-item">A fourth item</li>
-          <li className="list-group-item">And a fifth one</li>
+        {
+  items.map(item => (
+    <li key={item}>{item}</li>
+  ))
+}
+
         </ul>
     </Fragment>
   );
