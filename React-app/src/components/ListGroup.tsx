2eabfbdcf05267c37c9f5a4d50b3fe1f4724b29a
Kariuki11 <irungukenneth433@gmail.com>
2024-06-12 02:50:06 +0300

    new commits

--- React-app/src/components/ListGroup.tsx
+++ React-app/src/components/ListGroup.tsx
@@ -5,7 +5,7 @@ function ListGroup() {
     items = [];
 
     if (items.length === 0)
-        return <p>No item found</p>;
+        return<Fragment><h1>List</h1> <p>No item found</p>;</Fragment>
 
   return (
     <Fragment>
